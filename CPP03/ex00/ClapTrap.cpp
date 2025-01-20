3ff01f3f6edf6daef2862ee67d138c9e85ca4b53
Horio <arturjpm213@gmail.com>
2025-01-20 19:09:07 +0000

    CPP03

---
+++ CPP03/ex00/ClapTrap.cpp
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajorge-p <ajorge-p@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/20 17:56:40 by ajorge-p          #+#    #+#             */
+/*   Updated: 2025/01/20 19:06:31 by ajorge-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap() : hp(10), ep(10), ad(0){
+	
+}
+
+ClapTrap::ClapTrap(std::string name) : name(name), hp(10), ep(10), ad(0){
+	std::cout << "Constructor Called" << std::endl;
+}
+
+ClapTrap::~ClapTrap(){
+	std::cout << "Destructor Called" << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &copy){
+	std::cout << "Copy Constructor Called" << std::endl;
+	*this = copy;
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &copy){
+	if(this != &copy)
+	{
+		this->name = copy.name;
+		this->hp = copy.hp;
+		this->ep = copy.ep;
+		this->ad = copy.ad;	
+	}
+	return *this;
+}
+
+void ClapTrap::attack(const std::string& target){
+	if(this->ep > 0)
+	{
+		std::cout << "ClapTrap " << this->name << " attacks " << target << ", causing " << this->ad << " points of damage!" << std::endl;
+		this->ep--;
+	}
+	else
+		std::cout << "No Energy!!" << std::endl;
+}
+
+void ClapTrap::takeDamage(unsigned int amount){
+	this->hp -= amount;
+	this->ep--;
+	if(this->hp <= 0 || hp > 4000000000)
+		std::cout << "ClapTrap " << this->name << " is dead!!" << std::endl;
+	else
+		std::cout << "ClapTrap " << this->name << " took " << amount << " damage / HP = " << this->hp << std::endl;
+	
+}
+
+void ClapTrap::beRepaired(unsigned int amount){
+	if(this->ep > 0)
+	{
+		if(this->hp <= 0 || hp > 4000000000)
+			std::cout << "ClapTrap " << this->name << " is dead and cant be repaired" << std::endl;
+		else
+		{
+			std::cout << "ClapTrap " << this->name << " repaired himself for " << amount << " HP / HP = " << this->hp + amount << std::endl;
+			this->hp += amount;
+			this->ep--;
+		}
+			
+	}
+	else
+		std::cout << "No Energy!!" << std::endl;
+}
+
---
+++ CPP03/ex00/main.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajorge-p <ajorge-p@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/20 17:56:42 by ajorge-p          #+#    #+#             */
+/*   Updated: 2025/01/20 19:08:22 by ajorge-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int main(){
+
+	ClapTrap Jose("José");
+	ClapTrap Anibal("Aníbal");
+	ClapTrap Toze;
+	ClapTrap Antonio(Jose);
+
+	Toze = Anibal;
+
+	//Testes Normais
+	Jose.attack("Anibal");
+	Anibal.takeDamage(0);
+	Anibal.beRepaired(5);
+	Toze.attack("José");
+	Jose.takeDamage(5);
+	Jose.beRepaired(2);
+	Jose.attack("Anibal");
+	
+	//Toze e Antonio Morrem
+	Toze.takeDamage(15);
+	Antonio.takeDamage(11);
+	//Toze está morto mas quer reparar
+	Toze.beRepaired(10);
+
+	//Kyle Fica sem energia
+	ClapTrap Kyle("Kyle");
+	for(int i = 0; i <= 10 ; i++)
+		Kyle.attack("Anibal");
+	Kyle.beRepaired(1);
+}
