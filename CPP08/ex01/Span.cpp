695d5b69355ae5f88127ecba4dbd050bd90d2fc3
Artur Monteiro <47241810+Horioo@users.noreply.github.com>
2025-03-20 14:47:31 +0000

    Update Span.cpp

--- CPP08/ex01/Span.cpp
+++ CPP08/ex01/Span.cpp
@@ -10,3 +10,35 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Span.hpp"
+
+Span::Span(unsigned int n){
+  std::cout << "Span Constructor" << std::endl;
+  this.N = n;
+}
+
+Span::~Span(){
+  std::cout << "Span Destructor" << std::endl;
+}
+
+Span::Span(const Span &other){
+  std::cout << "Span Copy Constructor" << std::endl;
+  this->deq = other.deq;
+  this->N = other.N;
+}
+
+Span &Span::operator=(const Span &copy){
+  std::cout << "Span Copy Assignment" << std::endl;
+  if(this != &copy){
+      this->deq = copy.deq;
+      this->N = copy.N;
+  }
+  return *this;
+}
+
+Span::addNumber(unsigned int n){
+  if(deq.size() < this.N){
+    this->deq.push_back(n);
+  else
+      throw std::runtime_error("Tried to enter more than the Deq can store");
+}
